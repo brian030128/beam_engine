0d29ae1fb405ab6205ba31d53565084f5135a7eb
Brian <brian030128@gmail.com>
2026-01-03 00:59:55 +0800

    write to kv cache when decoding

--- src/beam_engine/beam.py
+++ src/beam_engine/beam.py
@@ -291,8 +291,8 @@ def demo_diverse_beam_search(model, tokenizer):
     # Example prompts
     prompts = [
         "The future of artificial intelligence is",
-        "Once upon a time in a magical forest,",
-        "The best way to solve climate change is"
+        #"Once upon a time in a magical forest,",
+        #"The best way to solve climate change is"
     ]
 
     for prompt in prompts:
