02208902cc5baedf3129e11bb2fd9759e270ed04
Brian <brian030128@gmail.com>
2026-01-04 00:50:37 +0800

    update

--- src/beam_engine/models/modeling_llama.py
+++ src/beam_engine/models/modeling_llama.py
@@ -328,7 +328,7 @@ def flashinfer_prefill_attention_forward(
     return attn_output, None  # FlashInfer doesn't return attention weights
 
 workspace_size = 128 * 1024 * 1024  # 128MB
-workspace_buffer = torch.empty(workspace_size, dtype=torch.uint8, device=device)
+workspace_buffer = torch.empty(workspace_size, dtype=torch.uint8, device="cuda:5")
 
 class LlamaAttention(nn.Module):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
