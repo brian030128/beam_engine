57351ab3bdf09acabd8cb361d5f6e01733f4298c
Brian Chan <brian030128@gmail.com>
2026-01-08 21:28:22 +0800

    update

--- src/beam_engine/models/modeling_llama.py
+++ src/beam_engine/models/modeling_llama.py
@@ -619,7 +619,9 @@ class LlamaModel(LlamaPreTrainedModel):
 
         # Initialize weights and apply final processing
         self.post_init()
-        self.head_dim**-0.5
+        self.head_dim = getattr(config, "head_dim", config.hidden_size // config.num_attention_heads)
+        self.num_key_value_groups = config.num_attention_heads // config.num_key_value_heads
+        self.scaling = self.head_dim**-0.5
 
     @check_model_inputs()
     @auto_docstring
